b7dea58fa4c7322253b0099c9cf1988630b696f7
Unknown <victor@pistache.co>
2018-08-19 17:40:50 -0300

    Add urls of views

---
+++ blog_project/blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from blog import views
+
+urlpatterns = [
+    path('',views.PostListViews.as_view(),name='post_list'),
+    path('about/',views.AboutViews.as_view(),name='about'),
+    path('post/?P<pk>\d+',views.PostDetailView.as_view(),name='post_detail'),
+    path('post/new/',views.CreatePostViews.as_view(),name='post_new'),
+    path('post/?P<pk>\d+/edit/',views.PostUpdateViews.as_view(),name='post_edit'),
+    path('post/(?P<pk>\d+/remove/',views.PostDeleteView.as_view(),name='post_remove'),
+    path('drafts/',views.DraftListView.as_view(),name='post_drafts_list'),
+]
