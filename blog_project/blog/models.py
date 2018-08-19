9c520bbabfc3a192b8d3e6e7fff185c391288ad4
Unknown <victor@pistache.co>
2018-08-19 16:17:26 -0300

    Reverse request method

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -16,8 +16,11 @@ class Post(models.Model):
         self.save()
     
     def approve_comments(self):
-        return self.comments.filter(approved_comments=True)
+        return self.comments.filter(approved_comment=True)
     
+    def get_absolute_url(self):
+        return reverse('post_detail',kwargs={'pk':self.pk})
+
     def __str__(self):
         return self.title
 
@@ -27,7 +30,6 @@ class Comment(models.Model):
     author = models.ForeignKey(max_length=200)
     text = models.TextField()
     create_date = models.DataTimeField(default=timezone.now())
-    approved_comment = models.BooleanField(default=False)    approved_comment = models.BooleanField(default=False)
     approved_comment = models.BooleanField(default=False)
 
     def approve(self):
