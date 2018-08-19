bc471c2cac4f7272140bc5e6201ef5857f26a53e
Unknown <victor@pistache.co>
2018-08-19 15:27:10 -0300

    install django in to dependencies

---
+++ django-blog/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/victor/Documents/me/projects/django_blog/django-blog/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
